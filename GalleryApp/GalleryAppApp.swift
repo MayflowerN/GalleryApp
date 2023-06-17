17c36e02c8893edfcb3c487834c403e4196491d5
EllieNguyen <ellieN1235@gmail.com>
2023-06-17 16:35:02 -0700

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Ellie on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
